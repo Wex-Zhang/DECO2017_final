3b2db637a70a751970cf5c1d9b2df25bf94be207
Wex <wex6899@gmail.com>
2023-06-07 09:19:16 +1000

    update responsive design and enter limit

--- public/script.js
+++ public/script.js
@@ -22,6 +22,27 @@ function closeInputBox(inputId) {
     inputBox.style.display = 'none';
 }
 
+function calculateTimePer100m(duration, distance) {
+    // Calculate the time in seconds for each 100 meters
+    let timePer100mInSeconds = (duration * 60 / distance) * 100;
+
+    // Convert the time to minutes and seconds
+    let minutes = Math.floor(timePer100mInSeconds / 60);
+    let seconds = Math.round(timePer100mInSeconds % 60);
+
+    // Format the time as a string
+    return minutes + "&apos;" + seconds + "&quot;/100m";
+}
+
+function formatDuration(durationInMinutes) {
+    // change the duration to hours and minutes
+    let hours = Math.floor(durationInMinutes / 60);
+    let minutes = durationInMinutes % 60;
+    if(hours < 10) hours = '0' + hours;
+    if(minutes < 10) minutes = '0' + minutes;
+
+    return hours + ":" + minutes;
+}
 
 function addSwim() {
 
@@ -31,6 +52,19 @@ function addSwim() {
     let met = parseFloat(document.getElementById('intensity').value);
     let date = document.getElementById('date').value;
     let calories = calculateCalories(userInfo.weight, met, duration);
+    let swimstyle = null;
+    let timePer100m = calculateTimePer100m(duration, distance);
+    let formattedDuration = formatDuration(duration);
+
+    if(met == 6){
+        swimstyle = 'Freestyle'
+    }else if (met == 8.3) {
+        swimstyle = 'Backstroke';
+    } else if (met == 9.8) {
+        swimstyle = 'Breaststroke';
+    } else if (met == 13.8) {
+        swimstyle = 'Butterfly stroke';
+    } 
 
     // check if user entered all value
     if (duration === "" || distance === "" || met === "" || date === "") {
@@ -42,12 +76,21 @@ function addSwim() {
         alert("Swim duration and distance should be positive value");
         return;
     }    
+    // check if user entered positive value
+    if (parseInt(duration) >= 800 || parseInt(distance) >= 9999) {
+        alert("Please enter your regular single swim data, regular swim duration should be smaller than 800 mins, distance should be smaller than 9999m,");
+        return;
+    }    
+
 
     let swimEntry = {
         duration: duration,
         distance: distance,
         calories: calories,
         date: date,
+        swimstyle: swimstyle,
+        timePer100m: timePer100m,
+        formattedDuration: formattedDuration
     };
 
     swimData.push(swimEntry);
@@ -64,20 +107,32 @@ function displaySwimEntry(entry, index) {
     card.className = 'card';
     card.innerHTML = `
         <div class="dataBoxOne">
-            <img src="timeIcon.png" class="dataIcon" alt="timeIcon">
-            <h1 class="cardDataOne">${entry.duration}</h1>
-            <h4 class="cardUnitOne">min</h4>
-        </div>
+            <img src="calorieIcon.png" class="dataIcon" alt="calorieIcon">
+            <h1 class="cardDataOne">${entry.calories}</h1>
+            <h4 class="cardUnitOne">kcal</h4>
+        </div>    
         <div class="dataBoxOne">
             <img src="distanceIcon.png" class="dataIcon" alt="distanceIcon">
             <h1 class="cardDataOne">${entry.distance}</h1>
             <h4 class="cardUnitOne">m</h4>
         </div>
-        <div class="dataBoxOne">
-            <img src="calorieIcon.png" class="dataIcon" alt="calorieIcon">
-            <h1 class="cardDataOne">${entry.calories}</h1>
-            <h4 class="cardUnitOne">kcal</h4>
-        </div>       
+        <div class = "cardVerticalDivider"></div>
+
+        <div class="dataBoxTwo">
+            <div>
+                <h1 class="cardDataTwo">${entry.swimstyle}</h1>
+                <h4 class="cardUnitTwo">Swim style</h4>
+            </div>
+            <div class="dataBoxTwoInner">
+                <h1 class="cardDataTwo">${entry.timePer100m}</h1>
+                <h4 class="cardUnitTwo">Avg. pace</h4>
+            </div>
+            <div>
+                <h1 class="cardDataTwo">${entry.formattedDuration}</h1>
+                <h4 class="cardUnitTwo">Time</h4>
+            </div>
+        </div>
+
         <div class="dateAndDeleteBox">
             <h4>${entry.date}</h4>
             <button class="deleteButton" onclick="deleteSwim(${index})">Delete</button>
